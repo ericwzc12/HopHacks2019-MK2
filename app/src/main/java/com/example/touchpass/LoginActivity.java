d22ff4a68fd88a23be2011eb4e093c537a6b48ae
mli25782 <mli25782@gmail.com>
2019-02-16 09:12:19 -0500

    volley and created simple request STILL NEED SERVER URL

--- app/src/main/java/com/example/touchpass/LoginActivity.java
+++ app/src/main/java/com/example/touchpass/LoginActivity.java
@@ -7,9 +7,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String server_url = "";
+    TextView textView = (TextView)findViewById(R.id.textView);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +29,27 @@ public class LoginActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String hash = intent.getStringExtra("hash");
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                final RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                        requestQueue.stop();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Something went wrong");
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                });
+                requestQueue.add(stringRequest);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
